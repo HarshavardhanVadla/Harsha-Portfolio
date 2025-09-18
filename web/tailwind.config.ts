4d7f201c49929d2cb73b91e9f72ab534cc0286d3
HARSHAVARDHAN VADLA <harshavardhanvadla6@gmail.com>
2025-09-18 12:43:06 -0400

    Update tailwind.config.ts

--- web/tailwind.config.ts
+++ web/tailwind.config.ts
@@ -1,9 +1,6 @@
-import type { Config } from "tailwindcss";
-
-const config: Config = {
+export default {
   darkMode: "class",
   content: ["./app/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}"],
   theme: { extend: {} },
   plugins: [],
 };
-export default config;
